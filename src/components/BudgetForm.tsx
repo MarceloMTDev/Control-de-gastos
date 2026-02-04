5e73157cddaee9efa9617d1e05008477dd625526
MarceloMTDev <20397755@uagro.mx>
2026-02-04 02:36:08 -0600

    Creando reducers para acciones y contexto para usar las acciones de forma global, también se creó un hook para el uso de las acciones del reducer

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,6 +1,48 @@
+import { useMemo, useState } from "react"
+import { useBudget } from "../hooks/useBudget"
 
 export default function BudgetForm() {
+
+  const [budget, setBudget] = useState(0)
+  const { dispatch } = useBudget()
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value)
+    setBudget(e.target.valueAsNumber)
+  }
+
+  const isValid = useMemo(() => {
+    return isNaN(budget) || budget <= 0
+  }, [budget])
+
+  const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    dispatch({type: 'add-budget', payload: {budget}})
+  }
   return (
-    <div>BudgetForm</div>
+    <form className="space-y-5" onSubmit={handleSubmit}>
+      <div className="flex flex-col space-y-5">
+        <label htmlFor="budget" className="text-4xl text-blue-600 font-bold text-center">
+          Definir presupuesto
+        </label>
+        <input
+          id="budget"
+          type="number"
+          className="w-full bg-white border border-gray-200 p-2"
+          placeholder="Define tu presupuesto"
+          name="budget"
+          value={isNaN(budget) ? "" : budget}
+          onChange={handleChange}
+        />
+      </div>
+
+      <input
+        type="submit"
+        value='Definir Presupuesto'
+        className="bg-blue-600 hover:bg-blue-700 disabled:opacity-40 cursor-pointer w-full p-2 text-white font-black"
+        disabled={isValid}
+      />
+    </form>
   )
 }
