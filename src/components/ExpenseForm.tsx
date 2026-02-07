de55b761d5ad4842ace6e4e9e1b5d6cf701e8c5d
MarceloMTDev <20397755@uagro.mx>
2026-02-06 19:13:45 -0600

    Agregando funcionalidad a BudgetTracker, cálculo de gastos y presupuesto, validaciones de gasto disponible

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -17,13 +17,14 @@ export default function ExpenseForm() {
     })
 
     const [error, setError] = useState('')
-
-    const { dispatch, state } = useBudget()
+    const [previousAmount, setPreviousAmount] = useState(0)
+    const { dispatch, state, remainingBudget } = useBudget()
 
     useEffect(() => {
         if (state.editingId) {
             const editingExpense = state.expenses.filter(currentExpense => currentExpense.id === state.editingId)[0]
             setExpense(editingExpense)
+            setPreviousAmount(editingExpense.amount)
         }
     }, [state.editingId])
 
@@ -52,6 +53,12 @@ export default function ExpenseForm() {
             return
         }
 
+        //Validar no sobrepasar el límite
+        if ((expense.amount - previousAmount) > remainingBudget) {
+            setError('Presupuesto sobrepasado')
+            return
+        }
+
         //Agregar o editar gasto
         if (state.editingId) {
             dispatch({ type: 'update-expense', payload: { expense: { id: state.editingId, ...expense } } })
@@ -67,6 +74,7 @@ export default function ExpenseForm() {
             category: '',
             date: new Date()
         })
+        setPreviousAmount(0)
     }
 
     return (
--- src/context/BudgetContext.tsx
+++ src/context/BudgetContext.tsx
@@ -1,9 +1,11 @@
-import { useReducer, createContext, type Dispatch, type ReactNode, } from "react"
+import { useReducer, createContext, type Dispatch, type ReactNode, useMemo, } from "react"
 import { budgetReducer, initialState, type BudgetActions, type BudgetState, } from "../reducers/budget-reducers"
 
 type BudgetContextProps = {
     state: BudgetState
     dispatch: Dispatch<BudgetActions>
+    totalExpenses: number
+    remainingBudget: number
 }
 
 type BudgetProviderProps = {
@@ -15,12 +17,16 @@ export const BudgetContext = createContext<BudgetContextProps>(null!)
 export const BudgetProvider = ({ children }: BudgetProviderProps) => {
 
     const [state, dispatch] = useReducer(budgetReducer, initialState)
+    const totalExpenses = useMemo(() => state.expenses.reduce((total, expenses) => expenses.amount + total, 0), [state.expenses])
+    const remainingBudget = state.budget - totalExpenses
 
     return (
         <BudgetContext.Provider
             value={{
                 state,
-                dispatch
+                dispatch,
+                totalExpenses,
+                remainingBudget
             }}>
 
             {children}
